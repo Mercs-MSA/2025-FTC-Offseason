6a9974e179be333169942b684eb03aa3d1be4a4d
Joshua Lee <joshua.l@mckinneysteamacademy.org>
2025-08-28 20:23:18 -0500

    more testing added

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestbedSwyftDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestbedSwyftDrive.java
@@ -3,21 +3,29 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+import org.firstinspires.ftc.robotcore.external.navigation.CurrentUnit;
 
 @TeleOp
 public class TestbedSwyftDrive extends OpMode {
 
-    private DcMotor swyftTest = null;
+    private DcMotorEx swyftTest = null;
     private double x = 0.0;
 
     @Override
     public void init() {
-        swyftTest = hardwareMap.get(DcMotor.class, "swyft");
+        swyftTest = hardwareMap.get(DcMotorEx.class, "swyft");
+
+        swyftTest.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
     }
 
     @Override
     public void loop() {
-        swyftTest.setPower(x);
         x = -gamepad1.left_stick_y;
+        swyftTest.setPower(x);
+
+        telemetry.addData("Power:", swyftTest.getPower());
+        telemetry.addData("Current:", swyftTest.getCurrent(CurrentUnit.AMPS));
     }
 }
